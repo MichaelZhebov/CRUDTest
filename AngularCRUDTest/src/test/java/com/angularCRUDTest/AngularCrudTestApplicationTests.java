822de304858088fc0c385bf4309fadc4f0c79080
MiMiSaR <mzhebov@gmail.com>
2020-02-29 14:07:12 +0200

    First commit

---
+++ AngularCRUDTest/src/test/java/com/angularCRUDTest/AngularCrudTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angularCRUDTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularCrudTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
